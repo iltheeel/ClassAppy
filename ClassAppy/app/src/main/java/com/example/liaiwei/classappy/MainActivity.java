c2786a5859b508f9dd9314b4cf5106c2a0b78fb1
iltheeel <liaiweiwei@gmail.com>
2017-06-05 08:03:15 -0700

    setup? idk what this is lol

---
+++ ClassAppy/app/src/main/java/com/example/liaiwei/classappy/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.liaiwei.classappy;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Toolbar;
+
+public class MainActivity extends ListActivity {
+
+    private ListView mListView;
+    String[] listItems = {
+            "Physics", "Biology", "Chemistry", "English", "Cooking", "Geometry", "Calculus I"
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        //create listview
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.list_fruit,listItems));
+
+        ListView listView = getListView();
+        listView.setTextFilterEnabled(true);
+
+        listView.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // When clicked, show a toast with the TextView text
+                Toast.makeText(getApplicationContext(),
+                        ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+}
